5b4f702080ee2e98b48ef3432705b9438b524f4c
Isaias Almeida <manoelisaiasa@gmail.com>
2019-12-06 21:35:47 -0300

    Configaçao de produçao

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(`$/dist/$`));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(`$/dist/$/index.html`));
+  res.sendFile(path.join(`$/dist/search-address/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
