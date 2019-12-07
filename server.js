079b3d74dc0a8a65847c3e0d38f239257d256ea5
Isaias Almeida <manoelisaiasa@gmail.com>
2019-12-06 21:44:58 -0300

    Configaçao de produçao

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const path = require("path");
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`$/dist/$`));
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// app.use(express.static(`$/dist/$`));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(`$/dist/search-address/index.html`));
