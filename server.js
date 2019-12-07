a49f075e99d6e7c21d1624ad939e6811cfbb018a
Isaias Almeida <manoelisaiasa@gmail.com>
2019-12-06 21:22:05 -0300

    Configaçao de produçao

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-//Install express server
 const express = require("express");
 const path = require("path");
-
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(`$/dist/$`));
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(`$/dist/$/index.html`));
 });
 
-// Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log("Express server listening on port", port);
-});
+app.listen(process.env.PORT || 8080);
